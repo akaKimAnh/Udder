bec91d9b4ebf7cc274428fa3f6f60dcc6fd9269f
akakimanh <kimanh.220102@gmail.com>
2023-06-19 12:49:34 +0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Kim Anh on 19/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
