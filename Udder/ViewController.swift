f175ed92253bb9eb7b82f00ef1200a52640bfa5b
akakimanh <kimanh.220102@gmail.com>
2023-06-19 13:04:55 +0700

    delete WillAppear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -14,8 +14,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        <#code#>
-    }
+    
 }
 
