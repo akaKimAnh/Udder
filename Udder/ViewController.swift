fe7c5eecec9ce60fd50b8b8eb180ae0d896e251b
akakimanh <kimanh.220102@gmail.com>
2023-06-19 13:12:40 +0700

    add all

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    
+    override func viewWillDisappear(_ animated: Bool) {
+        <#code#>
+    }
 }
 
